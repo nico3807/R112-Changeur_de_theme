bcfbb053616fcf4c8f9adf91193075265bbd5874
nico3807 <nicolas.maurin1@umontpellier.fr>
2025-11-04 13:55:24 +0100

    minification

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-// 1. Sélectionner le bouton par son ID
-const themeButton = document.getElementById("toggle-theme-btn");
-
-// 2. Sélectionner l'élément sur lequel appliquer la classe (le corps de la page)
-const bodyElement = document.querySelector("body");
-
-themeButton.addEventListener("click", function () {
-  // La logique de bascule sera insérée ici !
-  console.log("Bouton cliqué !"); // Pour vérifier que l'événement fonctionne
+document.addEventListener("contextmenu", (e) => e.preventDefault());
+const t = document.getElementById("toggle-theme-btn");
+const c = document.querySelector("body");
+t.addEventListener("click", function () {
+  console.log("Bouton cliqué!");
 });
-themeButton.addEventListener("click", function () {
-  // Toggle (bascule) : Si la classe est là, elle l'enlève. Si elle n'est pas là, elle l'ajoute.
-  bodyElement.classList.toggle("dark-theme");
-
-  // Bonus : Changer le texte du bouton pour indiquer l'action inverse
-  if (bodyElement.classList.contains("dark-theme")) {
-    themeButton.textContent = "Activer le Mode Clair";
+t.addEventListener("click", function () {
+  c.classList.toggle("dark-theme");
+  if (c.classList.contains("dark-theme")) {
+    t.textContent = "Activer le Mode Clair";
   } else {
-    themeButton.textContent = "Activer le Mode Sombre";
+    t.textContent = "Activer le Mode Sombre";
   }
 });
